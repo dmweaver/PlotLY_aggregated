63f7f7e7c551507f81fadb788f4ba0b8362c74f4
weaverd <david.weaver@dpird.wa.gov.au>
2022-11-09 21:42:52 +0800

    Basic Sum By Group

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-//Here we're importing items we'll need. You can add other imports here.
+import Plotly from "plotly.js/dist/plotly";
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+const subject = ['Low','Optimum','High','Low','Optimum','High','Low','Optimum','High','Low','Optimum','High']
+const score = [1,6,2,8,2,9,4,5,1,5,2,8]
+
+const data = [{
+  marker:{
+    color: ['red', 'orange', 'green']
+  },
+  type: 'bar',
+  x: subject,
+  y: score,
+  mode: 'markers',
+  transforms: [{
+    type: 'aggregate',
+    groups: subject,
+    aggregations: [
+      {target: 'y', func: 'sum', enabled: true},
+    ]
+  }]
+}];
+const layout = {
+  title: 'Phosphorus Status',
+  width: 800,
+  height: 600,
+  font: {size: 14},
+  xaxis: {linewidth: 2, showgrid: 'major'},
+  yaxis: {linewidth: 2}
 };
+Plotly.newPlot('chart', data, layout, {displayModeBar: false});
 
-//
